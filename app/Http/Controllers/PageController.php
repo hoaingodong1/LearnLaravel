f8227c780405aed76713e16f2e4e18e65f8abee9
hoaingodong1 <hoai.ngo23@student.passerellesnumeriques.org>
2022-06-24 08:08:39 +0700

    cut

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,15 +4,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Facades\Date;
 use App\Models\Slide;
+use App\Models\Product;
 class PageController extends Controller
 {
-    //
-    // public function getIndex(){
-    //     return view('page.trangchu');
-    // }
-    public function getIndex(){
-    
-    $slide= Slide::all();    
-    return view('page.trangchu',compact('slide',));
+    public function getIndex(){    
+    $slide= Slide::all();       
+    $new_product=Product::where('new',1)->get();
+    return view('page.trangchu',compact('slide','new_product'));
 }
 }
